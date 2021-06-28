8ad2e48c4aab8ef528b7b135379324e456fb61f7
astinravidahal <44469638+astin2044327@users.noreply.github.com>
2021-06-28 18:30:27 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,15 @@
+
+provider "aws" {
+  profile = "default"
+  region= "us-east-1"
+}
+
+
+
+
 #creating aws instance
 resource "aws_instance" "ec2-01" {
-    ami= "ami-0d5eff06f840b45e9"
-    region= "us-east-1"
-  
+    ami= "ami-0d5eff06f840b45e9"  
     instance_type = "t2.micro"
     tags ={
         Name= "Jenkins-demo"
